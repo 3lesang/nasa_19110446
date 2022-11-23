52d74c111ae5b3dee74d924a46d7fb28be869899
Minh Sang <msang.sg@gmail.com>
2022-11-23 08:08:58 +0700

    fixed

--- server/test/index.js
+++ server/test/index.js
@@ -1,38 +1,38 @@
-const request = require('supertest');
-const app = require('../src/app');
+const request = require("supertest");
+const app = require("../src/app");
 
-describe('GET /planets', function () {
-    it('responds with json', function (done) {
+describe("GET /planets", function () {
+    it("responds with json", function (done) {
         request(app)
-            .get('/planets')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
+            .get("/planets")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
             .expect(200, done);
     });
 });
 
-describe('GET /launches', function () {
-    it('responds with json', function (done) {
+describe("GET /launches", function () {
+    it("responds with json", function (done) {
         request(app)
-            .get('/launches')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
+            .get("/launches")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
             .expect(200, done);
     });
 });
 
-describe('POST /launches', function () {
-    it('responds with json', function (done) {
+describe("POST /launches", function () {
+    it("responds with json", function (done) {
         request(app)
-            .post('/launches')
+            .post("/launches")
             .send({
-                mission: 'Exploration kepler-x',
-                rocket: 'Explorer',
-                launchDate: '27 December, 2030',
-                target: 'kepler',
+                mission: "Exploration kepler-x",
+                rocket: "Explorer",
+                launchDate: "27 December, 2030",
+                target: "kepler",
             })
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
             .expect(201)
             .end(function (err, res) {
                 if (err) return done(err);
@@ -41,12 +41,12 @@ describe('POST /launches', function () {
     });
 });
 
-describe('DELETE /launches/:id', function () {
-    it('responds with json', function (done) {
+describe("DELETE /launches/:id", function () {
+    it("responds with json", function (done) {
         request(app)
-            .delete('/launches/100')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
+            .delete("/launches/100")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
             .expect(200)
             .end(function (err, res) {
                 if (err) return done(err);
